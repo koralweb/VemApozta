16c6f58eb0b929727d7572297766dbeb86f1964e
koralweb <dev@koralweb.com>
2024-04-08 09:36:19 +0300

    add navigation

---
+++ src/screens/HomeScreen.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import {StyleSheet, Text, View} from 'react-native';
+
+const HomeScreen = () => {
+  return (
+    <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+      <Text>Home Screen</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({});
+
+export default HomeScreen;
